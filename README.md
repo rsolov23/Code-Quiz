fe21d3a119f293edd9bd3bfc59d5b77f28e4ff9d
Rachel Solov <rsolov23@yahoo.com>
2021-04-16 08:39:33 -0500

    working on localStorage

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Code-Quiz-2
+![A screenshot of the web page](/assets/images/screenshot.png)
+
+# Code Quiz
+
+## Table of Contents
+
+- [Description](#description)
+- [Instructions](#instructions)
+- [Github Profile](#github-profile)
+- [Link to Code Quiz](#link-to-code-quiz)
+- [Contributors](#contributors)
+
+### Description
+
+This is a Javascript Quiz.
+
+### Instructions
+
+1.
+2.
+3.
+4.
+5.
+6.
+7.
+
+### Github Profile
+
+https://github.com/rsolov23/
+
+### Link to my Password Generator
+
+https://
+
+### Contributors
+
+- Rachel Solov
+- Special shout out to Sean, Donte, Brendan, Megan, Tori, Mike, Bradley and all my classmates.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,75 @@
 // variables for questions
 let questions = [
-    {
-        question: 'A plain language description of the steps that an algorithm or application must complete is called',
-        answerA: 'A) an operation',
-        answerB: 'B) pseudocoding',
-        answerC: 'C) an array',
-        answerD: 'D) a condition',
-        crcAnswer: 'B) pseudocoding'
-    },
-    {
-        question: 'What element is used to incorporate JavaScript into HTML?',
-        answerA: 'A) <java>',
-        answerB: 'B) <body>',
-        answerC: 'C) <script>',
-        answerD: 'D) <header>',
-        crcAnswer: 'C) <script>'
-    }, 
-     {
-         question: 'Which of the following is NOT an operator?',
-        answerA: 'A) @',
-        answerB: 'B) !==',
-         answerC: 'C) ===',
-         answerD: 'D) <=',
-         crcAnswer: 'A) @'
-     },  
-     {
-         question: 'How do you convert an array into a string for saving in localStorage?',
-         answerA: 'A) string<>',
-         answerB: 'B) map()',
-         answerC: 'C) Number()',
-         answerD: 'D) stringify()',
-         crcAnswer: 'D) stringify()'
-     },  
-     {
-         question: 'What is a means of organizing and structuring data thats transferred from one place to another?',
-         answerA: 'A) callback function',
-         answerB: 'B) JSON',
-         answerC: 'C) localStorage',
-         answerD: 'D) .querySelector',
-         crcAnswer: 'B) JSON'
-     },  
-     {
-         question: 'Which of the following is NOT a falsy value',
-         answerA: 'A) console.log',
-         answerB: 'B) 0',
-         answerC: 'C) Null',
-         answerD: 'D) NaN',
-         crcAnswer: 'A) console.log'
-     },
-]
+  {
+    question:
+      "A plain language description of the steps that an algorithm or application must complete is called",
+    answerA: "A) an operation",
+    answerB: "B) pseudocoding",
+    answerC: "C) an array",
+    answerD: "D) a condition",
+    crcAnswer: "B) pseudocoding",
+  },
+  {
+    question: "What element is used to incorporate JavaScript into HTML?",
+    answerA: "A) <java>",
+    answerB: "B) <body>",
+    answerC: "C) <script>",
+    answerD: "D) <header>",
+    crcAnswer: "C) <script>",
+  },
+  {
+    question: "Which of the following is NOT an operator?",
+    answerA: "A) @",
+    answerB: "B) !==",
+    answerC: "C) ===",
+    answerD: "D) <=",
+    crcAnswer: "A) @",
+  },
+  {
+    question:
+      "How do you convert an array into a string for saving in localStorage?",
+    answerA: "A) string<>",
+    answerB: "B) map()",
+    answerC: "C) Number()",
+    answerD: "D) stringify()",
+    crcAnswer: "D) stringify()",
+  },
+  {
+    question:
+      "What is a means of organizing and structuring data thats transferred from one place to another?",
+    answerA: "A) callback function",
+    answerB: "B) JSON",
+    answerC: "C) localStorage",
+    answerD: "D) .querySelector",
+    crcAnswer: "B) JSON",
+  },
+  {
+    question: "Which of the following is NOT a falsy value",
+    answerA: "A) console.log",
+    answerB: "B) 0",
+    answerC: "C) Null",
+    answerD: "D) NaN",
+    crcAnswer: "A) console.log",
+  },
+];
 
 var btnContainer = document.querySelector(".start-button");
 var startBtn = document.querySelector("#startQuiz");
 var quizContainer = document.querySelector(".quizContainer");
-var element = document.getElementById('start-content');
-var startContent = document.getElementById('start-content');
-var quizScore = document.getElementById('quizScore');
-var endScore = document.getElementById('endScore');
-var submitQuizScore = document.getElementById('submit-quiz-score');
-var enterInitials = document.getElementById('enter-initials');
-var initials =document.getElementById('initials')
+var element = document.getElementById("start-content");
+var startContent = document.getElementById("start-content");
+var quizScore = document.getElementById("quizScore");
+var endScore = document.getElementById("endScore");
+var questionsContent = document.getElementById("questionsContent");
+var submitQuizScore = document.getElementById("submit-quiz-score");
+var enterInitials = document.getElementById("enter-initials");
+var initials = document.getElementById("initials");
+var scoreInitials = document.getElementById("HS-initials");
+var highScore = document.getElementById("high-score-list");
+var timeRemaining = document.getElementById("timeRemaining");
+var questionAnswer = document.getElementById("questionAnswer");
+var time = document.querySelector(".time");
+let timer = document.getElementById("timeRemaining").value;
 
 //remaining variables
 
@@ -67,75 +77,115 @@ let currentQuestion;
 let countdown;
 
 //new question/starts quiz
-function beginQuiz(){
-    startContent.style.display="none";
-    currentQuestion = 0;
-    countdown = 180;
-    btnContainer.setAttribute("style", "display: none;")
-    quizContainer.setAttribute("style", "display: block;")
-    populateQuestions()
-    timerStart()
-
+function beginQuiz() {
+  startContent.style.display = "none";
+  currentQuestion = 0;
+  countdown = 180;
+  btnContainer.setAttribute("style", "display: none;");
+  quizContainer.setAttribute("style", "display: block;");
+  populateQuestions();
+  timerStart();
 }
 
-function populateQuestions(){
-    document.querySelector("#question").textContent = questions[currentQuestion].question
-    document.querySelector("#A").textContent = questions[currentQuestion].answerA
-    document.querySelector("#B").textContent = questions[currentQuestion].answerB
-    document.querySelector("#C").textContent = questions[currentQuestion].answerC
-    document.querySelector("#D").textContent = questions[currentQuestion].answerD
+function populateQuestions() {
+  document.querySelector("#question").textContent =
+    questions[currentQuestion].question;
+  document.querySelector("#A").textContent = questions[currentQuestion].answerA;
+  document.querySelector("#B").textContent = questions[currentQuestion].answerB;
+  document.querySelector("#C").textContent = questions[currentQuestion].answerC;
+  document.querySelector("#D").textContent = questions[currentQuestion].answerD;
 }
 
 //timer function
 
-function timerStart () {
-    let quizInterval = setInterval(function(){
-        countdown--;
-        document.querySelector("#time").textContent = countdown;
+function timerStart() {
+  let quizInterval = setInterval(function () {
+    countdown--;
+    document.querySelector("#time").textContent = countdown;
 
-        if(countdown<=0 || currentQuestion >= questions.length){
-            clearInterval(quizInterval)
-        }
-    }, 1000)
+    if (countdown <= 0 || currentQuestion >= questions.length) {
+      clearInterval(quizInterval);
+      // enterScore();
+    }
+  }, 1000);
 }
 
+let crcAnswer;
 //right answer/wrong answer with time deduction
-function checkAnswerAndIterate(event){
-    // console.log(this)
-    let choice = event.target.textContent
-    console.log(choice)
+function checkAnswerAndIterate(event) {
+  // console.log(this)
+  let choice = event.target.textContent;
+  console.log(choice);
 
-    if(choice == questions[currentQuestion].crcAnswer){
-        alert('Right✅')
-    }else{
-        alert('Wrong❌')
-        // countdown = countdown - 15
-        countdown -= 15;
-    }
+  if (choice == questions[currentQuestion].crcAnswer) {
+    alert("Right✅");
+  } else {
+    alert("Wrong❌");
+    // countdown = countdown - 15
+    countdown -= 15;
+  }
 
-    currentQuestion++
-    if(currentQuestion< questions.length){
-        populateQuestions()
-    }
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    populateQuestions();
+  }
 }
-// enter score
+
 function enterScore() {
-    quizContainer.style.display="none";
-
-};
-//save initials and score
-function saveToLocal () {
-    localStorage.setItem("quizScore", JSON.stringify([""]))
-    console.log(JSON.parse(localStorage.getItem("quizScore")),
-    localStorage.getItem("quizScore"))
+  questionAnswer.style.display = "none";
+  quizScore.style.display = "block";
+  clearInterval(quizInterval);
+  timer.textContent = "Game Over";
+  document.getElementById("finalScore").innerHTML =
+    "your score is" + "scoreListItem";
 }
-saveToLocal()
 
-//display high scores with local storage
+function createScoreList() {
+  let scoreList = Object.values(localStorage);
+  for (let i = 0; i < scoreList.length; i++) {
+    var scoreListItem = document.createElement("li");
+    scoreListItem.innerText = scoreList[i];
+    document.getElementById("high-scores-list").appendChild(scoreListItem);
+  }
+}
 
+function toggleScoreList() {
+  for (let i = 0; i < document.getElementsByTagName("*"); i++) {
+    document
+      .getElementsByTagName("*")
+      [i].setAttribute("style", "display: none;");
+    console.log();
+  }
+
+  document
+    .getElementById("high-scores-list")
+    .setAttribute("style", "display: block;");
+  createScoreList();
+}
+
+function submitInitials() {
+  if (checkAnswerAndIterate >= "6") {
+    onclick.addEventListener("submit").btn;
+  }
+}
+function saveToLocal(event) {
+  event.preventDefault();
+  debugger;
+  var timer = event.target.children[1].value;
+  // let timer = document.getElementById("timeRemaining").value;
+  let finalScore = { score: time };
+  JSON.parse(localStorage.getItem("timeRemaining"));
+  localStorage.getItem("timeRemaining");
+  localStorage.setItem("timeRemaining", time);
+  debugger;
+}
 //event listeners
-startBtn.addEventListener("click", beginQuiz)
-document.querySelector("#A").addEventListener("click", checkAnswerAndIterate)
-document.querySelector("#B").addEventListener("click", checkAnswerAndIterate)
-document.querySelector("#C").addEventListener("click", checkAnswerAndIterate)
-document.querySelector("#D").addEventListener("click", checkAnswerAndIterate)
+startBtn.addEventListener("click", beginQuiz);
+document.querySelector("#A").addEventListener("click", checkAnswerAndIterate);
+document.querySelector("#B").addEventListener("click", checkAnswerAndIterate);
+document.querySelector("#C").addEventListener("click", checkAnswerAndIterate);
+document.querySelector("#D").addEventListener("click", checkAnswerAndIterate);
+subScore.addEventListener("submit", saveToLocal);
+document
+  .getElementById("high-score-toggle")
+  .addEventListener("click", toggleScoreList);
